280d82554c26f87f5720fedee697e92503e63dd4
Aavesh Khan <aaveshkhan@macs-MacBook-Pro.local>
2025-12-25 17:27:21 +0530

    Fix: Configure Vercel Postgres support and migration route

--- config/database.php
+++ config/database.php
@@ -66,16 +66,16 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', env('POSTGRES_HOST', '127.0.0.1')),
+            'port' => env('DB_PORT', env('POSTGRES_PORT', '5432')),
+            'database' => env('DB_DATABASE', env('POSTGRES_DATABASE', 'forge')),
+            'username' => env('DB_USERNAME', env('POSTGRES_USER', 'forge')),
+            'password' => env('DB_PASSWORD', env('POSTGRES_PASSWORD', '')),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
